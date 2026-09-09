ccc2203f041e59ae45806b6f3589f96fa86dc3cd
mattiamion91 <mattia.mion@gmail.com>
2026-09-09 18:07:50 +0200

    feat: complete CRUD  scenario

--- src/main/java/todolist/TodoController.java
+++ src/main/java/todolist/TodoController.java
@@ -1,12 +1,17 @@
 package todolist;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+
+
 
 @RestController
 public class TodoController {
@@ -29,5 +34,32 @@ public class TodoController {
         listaTodo.add(nuovoTodo);
         return nuovoTodo;
     }
+
+    @GetMapping("/todos{id}")
+    public Todo getUno (@PathVariable Long id) {
+        for (Todo t : listaTodo) {
+            if (t.getId().equals(id)) {
+                return t;
+            }
+        }
+        return null;
+    }
+
+    @PutMapping("/todos{id}")
+    public Todo aggiorna(@PathVariable Long id, @RequestBody Todo datiAggiornati) {
+        Todo esistente = getUno(id);
+        if(esistente != null) {
+            esistente.setCompletato(datiAggiornati.isCompletato());
+        }
+        return esistente;
+    }
+
+    @DeleteMapping("/todos{id}")
+    public void elimina(@PathVariable Long id) {
+      Todo daRimuovere = getUno(id);
+      if(daRimuovere != null) {
+        listaTodo.remove(daRimuovere);
+      }
+    } 
 
 }

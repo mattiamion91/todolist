10580ea9e125f94fa192c7a634f6a4cc0496f3bc
mattiamion91 <mattia.mion@gmail.com>
2026-09-09 17:08:50 +0200

    feat: first endpoint created

---
+++ src/main/java/todolist/HelloController.java
@@ -0,0 +1,13 @@
+package todolist;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController 
+
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Ciao! il mo primo endpoint funziona";
+    }
+}

75e7cabf2d0d788e34d1a0ef4ab48def2ca5f536
mattiamion91 <mattia.mion@gmail.com>
2026-09-09 17:17:17 +0200

    feat: endpoint with variable path, url parameters

--- src/main/java/todolist/HelloController.java
+++ src/main/java/todolist/HelloController.java
@@ -2,12 +2,20 @@ package todolist;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController 
 
 public class HelloController {
+    
     @GetMapping("/hello")
     public String hello() {
-        return "Ciao! il mo primo endpoint funziona";
+        return "Ciao! il mio primo endpoint funziona";
     }
+
+    @GetMapping("/saluta/{nome}")
+    public String saluta(@PathVariable String nome) {
+        return "Ciao " + nome + "! Benvenuto in SpringBoot.";
+    }    
+
 }

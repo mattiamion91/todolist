4e7f71870f484aa78b148df0799805d116b7bb0d
mattiamion91 <mattia.mion@gmail.com>
2026-09-10 09:39:02 +0200

    feat: update TodoController,java with jpa syntax and linkend to H2 database

--- src/main/java/todolist/Todo.java
+++ src/main/java/todolist/Todo.java
@@ -1,13 +1,24 @@
 package todolist;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity 
 public class Todo {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
 
     private Long id;
     private String titolo;
     private Boolean completato;
+
+    public Todo() {
+    }
     
     public Todo(Long id, String titolo, boolean completato) {
-        this.id = id;
         this.titolo = titolo;
         this.completato = completato;
     }
@@ -18,6 +29,10 @@ public class Todo {
     
     public String getTitolo() {
         return titolo;
+    }
+
+    public void setTitolo(String titolo) {
+        this.titolo = titolo;
     }
     
     public Boolean isCompletato() {
--- src/main/java/todolist/TodoController.java
+++ src/main/java/todolist/TodoController.java
@@ -1,6 +1,13 @@
 package todolist;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+/*import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,58 +17,60 @@ import java.util.ArrayList;
 import java.util.List;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PutMapping;*/
 
 @RestController
 public class TodoController {
 
-    private List<Todo> listaTodo = new ArrayList<>();
+    @Autowired
+    private TodoRepository todoRepository;
 
-    public TodoController() {
-        listaTodo.add(new Todo(1L, "Comprare patatine", false));
-        listaTodo.add(new Todo(2L, "portare cane veterinario", true));
-        listaTodo.add(new Todo(3L, "studiare react.js", false));
-    }
+    /* private List<Todo> listaTodo = new ArrayList<>(); */
+
+    /*
+     * public TodoController() {
+     * listaTodo.add(new Todo(1L, "Comprare patatine", false));
+     * listaTodo.add(new Todo(2L, "portare cane veterinario", true));
+     * listaTodo.add(new Todo(3L, "studiare react.js", false));
+     * }
+     */
 
     @GetMapping("/todos")
     public List<Todo> getTutti() {
-        return listaTodo;
-    }
-
-    @PostMapping("/todos")
-    public Todo creaTodo(@RequestBody Todo nuovoTodo) {
-        listaTodo.add(nuovoTodo);
-        return nuovoTodo;
+        return todoRepository.findAll();
     }
 
     @GetMapping("/todos/{id}")
     public ResponseEntity<Todo> getUno(@PathVariable Long id) {
-        for (Todo t : listaTodo) {
-            if (t.getId().equals(id)) {
-                return ResponseEntity.ok(t);
-            }
+        Optional<Todo> risultato = todoRepository.findById(id);
+        if (risultato.isPresent()) {
+            return ResponseEntity.ok(risultato.get());
         }
         return ResponseEntity.notFound().build();
     }
 
+    @PostMapping("/todos")
+    public Todo creaTodo(@RequestBody Todo nuovoTodo) {
+        return todoRepository.save(nuovoTodo);
+    }
+
     @PutMapping("/todos/{id}")
     public ResponseEntity<Todo> aggiorna(@PathVariable Long id, @RequestBody Todo datiAggiornati) {
-        ResponseEntity<Todo> risposta = getUno(id);
-        if (risposta.getStatusCode().is2xxSuccessful()) {
-            Todo esistente = risposta.getBody();
+        Optional<Todo> risultato = todoRepository.findById(id);
+        if (risultato.isPresent()) {
+            Todo esistente = risultato.get();
             esistente.setCompletato(datiAggiornati.isCompletato());
+            todoRepository.save(esistente);
             return ResponseEntity.ok(esistente);
         }
         return ResponseEntity.notFound().build();
     }
 
     @DeleteMapping("/todos/{id}")
-    public ResponseEntity<Void> elimina(@PathVariable Long id) {
-        ResponseEntity<Todo> risposta = getUno(id);
-        if (risposta.getStatusCode().is2xxSuccessful()) {
-            listaTodo.remove(risposta.getBody());
-            return ResponseEntity.noContent().build();
+    public ResponseEntity<Void> elimina(@PathVariable Long id) {        
+        if (todoRepository.existsById(id)) {
+           todoRepository.deleteById(id);
+           return ResponseEntity.noContent().build();
         }
         return ResponseEntity.notFound().build();
     }

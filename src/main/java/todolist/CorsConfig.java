ed2ac1316bc9a0cd693784bcb2730e2b38acf261
mattiamion91 <mattia.mion@gmail.com>
2026-09-10 16:03:57 +0200

    feat: added cors for backend in render.com

--- src/main/java/todolist/CorsConfig.java
+++ src/main/java/todolist/CorsConfig.java
@@ -14,7 +14,9 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins(
+                                "http://localhost:5173",
+                                "https://todolist-react-client.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE");
             }
         };
